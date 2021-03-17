16029d1e385ac591ae04501189e536dc65c10f4c
Anna <garsinaanna32778@gmail.com>
2021-03-17 13:17:37 +0200

    Updates

---
+++ precache-manifest.5ccfe3d521053499d5768c0cdd8014d0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "951e1784217ecf91f2d05cc93616665b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "df171c63963d73b20efd",
+    "url": "./static/css/2.f6a27176.chunk.css"
+  },
+  {
+    "revision": "576f338b3e1b01230aa5",
+    "url": "./static/css/main.787ad92b.chunk.css"
+  },
+  {
+    "revision": "df171c63963d73b20efd",
+    "url": "./static/js/2.cd70d490.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.cd70d490.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "576f338b3e1b01230aa5",
+    "url": "./static/js/main.533e4932.chunk.js"
+  },
+  {
+    "revision": "7859211cf5cc2a17445c",
+    "url": "./static/js/runtime-main.2d746ae6.js"
+  }
+]);
